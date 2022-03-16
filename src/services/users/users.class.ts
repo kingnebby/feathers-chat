196c90dc5f815c9f01c90749375ba757956fb46b
Dan Villa <danielavilla02@gmail.com>
2022-03-15 22:44:58 -0700

    chore: generating an app

---
+++ src/services/users/users.class.ts
@@ -0,0 +1,45 @@
+import crypto from 'crypto';
+import { Params } from '@feathersjs/feathers';
+import { Service, NedbServiceOptions } from 'feathers-nedb';
+import { Application } from '../../declarations';
+
+interface UserData {
+  _id?: string;
+  email: string;
+  password: string
+  name?: string
+  avatar?: string
+  githubId?: string
+}
+
+export class Users extends Service<UserData> {
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars
+  constructor(options: Partial<NedbServiceOptions>, app: Application) {
+    super(options);
+  }
+
+  create(data: UserData, params?: Params) {
+    const {email, name, password, githubId} = data;
+    const avatar = data.avatar || getGravatar(email);
+    const userData = {
+      email,
+      name,
+      password,
+      githubId,
+      avatar
+    };
+    return super.create(userData, params);
+  }
+
+  // What about mapping other routes?
+  doSomething() {
+    return;
+  }
+}
+
+const gravatarUrl = 'https://s.gravatar.com/avatar';
+const query = 's=60';
+function getGravatar(email: string) {
+  const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+  return `${gravatarUrl}/${hash}?${query}`;
+}
