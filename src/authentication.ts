9abaa777026b065040cce8f637e5ffa31195c661
Dan Villa <danielavilla02@gmail.com>
2022-06-22 08:42:42 -0500

    feat: enable github login oauth

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,7 +1,7 @@
-import { ServiceAddons } from '@feathersjs/feathers';
+import { Params, ServiceAddons } from '@feathersjs/feathers';
 import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
 import { LocalStrategy } from '@feathersjs/authentication-local';
-import { expressOauth } from '@feathersjs/authentication-oauth';
+import { expressOauth, OAuthProfile, OAuthStrategy } from '@feathersjs/authentication-oauth';
 
 import { Application } from './declarations';
 
@@ -11,12 +11,28 @@ declare module './declarations' {
   }
 }
 
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile: OAuthProfile, existingEntity: any, params: Params): Promise<{ [x: string]: any; }> {
+    const baseData = await super.getEntityData(profile, existingEntity, params);
+    return {
+      ...baseData,
+      name: profile.login,
+      avatar: profile.avatar_url,
+      email: profile.email
+    };
+  }
+
+}
+
 export default function(app: Application): void {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
+  // http://localhost:3030/oauth/github
+  authentication.register('github', new GitHubStrategy());
 
+  // Here we bind the authentication service to the endpoint `authentication`
   app.use('/authentication', authentication);
   app.configure(expressOauth());
 }
